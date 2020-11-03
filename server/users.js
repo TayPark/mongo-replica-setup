4565d6e880ced8b80dec1206ed004a070640f392
chisacam <dladsds123@gmail.com>
2020-11-04 03:30:37 +0900

    fix schema

--- server/users.js
+++ server/users.js
@@ -1,3 +1,3 @@
 let mongoose = require('mongoose');
-let mycollectionSchema = new mongoose.Schema({name : String, fav: String});
+let mycollectionSchema = new mongoose.Schema({msg:String});
 module.exports = mongoose.model('User',mycollectionSchema);
